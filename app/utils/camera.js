8b9a073e863e96a3f541612d804e5a40f2a64100
dmitriyilchenko <dilchenko@zaraffasoft.com>
2020-07-08 15:04:18 +0300

    Handling camera in y offset.

--- app/utils/camera.js
+++ app/utils/camera.js
@@ -2,13 +2,13 @@ export default (entities, {screen}) => {
   let player = entities.player;
   let camera = entities.camera;
 
-  // let anchorY = screen.height * 0.65;
+  let anchorY = screen.height * 0.7;
   let anchorX = screen.width * 0.5;
 
-  // let diffY = anchorY - player.body.position.y - camera.offsetY;
+  let diffY = anchorY - player.body.position.y - camera.offsetY;
   let diffX = anchorX - player.body.position.x - camera.offsetX;
 
-  // camera.offsetY += diffY * 0.05;
+  camera.offsetY += diffY * 0.05;
   camera.offsetX += diffX * 0.15;
 
   return entities;
